060a301a85371dc943aa13cdfeefcff8e5c9eca5
Tristan Panco <tristan@Tristans-MacBook-Pro.local>
2022-12-04 19:09:16 +0200

    plugins

--- config/plugins.js
+++ config/plugins.js
@@ -2,5 +2,9 @@ module.exports = ({ env }) => ({
 	"content-versioning": {
 		enabled:  true,
 	},
-  'duplicate-button': true
+  'duplicate-button': true,
+	ckeditor: true,
+	seo: {
+    enabled: true,
+  },
 });
